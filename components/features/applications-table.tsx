18b682d1a73eafe207aa9a9a4e67e24ba66d57a9
mmed <mila.medic2000@gmail.com>
2026-05-22 16:02:50 +1000

    add delete application feature

--- components/features/applications-table.tsx
+++ components/features/applications-table.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Table,
   TableBody,
@@ -6,48 +8,55 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import applications from "@/data/applications.json";
+import { Button } from "../ui/button";
+import { deleteApplication } from "@/lib/actions";
 
 const columnHeader = [
-    "Company",
-    "Role Title",
-    "Type",
-    "Location",
-    "Deadline",
-    "Eligibility",
-    "Eligibility Reasoning",
-    "Salary",
-    "Link",
-    "Notes",
-    "Status",
-]
-
+  "Company",
+  "Role Title",
+  "Type",
+  "Location",
+  "Deadline",
+  "Eligibility",
+  "Eligibility Reasoning",
+  "Salary",
+  "Link",
+  "Notes",
+  "Status",
+];
 
 export default function ApplicationsTable() {
-
-    return (
-      <Table>
-        <TableCaption>A list of your applications.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            {columnHeader.map((ch) => (
-              <TableHead className="w-[100px]" key={ch}>
-                {ch}
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {applications.applications.map((app) =>
-          <TableRow>
+  return (
+    <Table>
+      <TableCaption>A list of your applications.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columnHeader.map((ch) => (
+            <TableHead className="w-[100px]" key={ch}>
+              {ch}
+            </TableHead>
+          ))}
+          {/**TODO: update this when connecting to database */}
+          <TableHead className="w-[100px]">Action</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {applications.applications.map((app) => (
+          <TableRow key={app.company}>
             {Object.entries(app).map(([key, value]) => (
-                <TableCell className={key}>{value}</TableCell>
+              <TableCell key={value}>{value}</TableCell>
             ))}
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    );
-    }
-
+            {/**TODO: update this when connecting to database */}
+            <TableCell className={app.company}>
+              <Button onClick={() => deleteApplication(app.company)}>
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
--- lib/actions.ts
+++ lib/actions.ts
@@ -2,16 +2,65 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import applications from "@/data/applications.json";
+import fs from "fs";
+import path from "path";
+
+const applicationsFilePath = "data/applications.json";
+
+{/**TODO: add id for each new application */}
 
 export async function createApplication(formData: FormData) {
 
- 
-  console.log(formData);
+
+  // read file
+  const file = fs.readFileSync(applicationsFilePath, "utf8");
+
+  // parse JSON
+  const jsonData = JSON.parse(file);
+
+  // create new json object
+  const newApplication = Object.fromEntries(formData.entries());
+
+  // insert object into json file
+  jsonData.applications.push(newApplication);
   
-//   revalidatePath("/dashboard/invoices");
+
+  // save file
+  fs.writeFileSync(applicationsFilePath, JSON.stringify(jsonData, null, 2));
+
+
+ 
+  console.log("created new applciation!");
+  
+  revalidatePath("/applications");
   redirect("/applications");
 
-  // Test it out:
-  console.log(formData);
+  
 }
+
+
+export async function deleteApplication(appCompany: string ) {
+
+
+
+  // read file
+  const file = fs.readFileSync(applicationsFilePath, "utf8");
+
+  // parse JSON
+  const jsonData = JSON.parse(file);
+
+  // delete applicaiton object from json
+  jsonData.applications = jsonData.applications.filter(
+    (app: any) => app.company != appCompany
+  );
+
+  // save file
+  fs.writeFileSync(applicationsFilePath, JSON.stringify(jsonData, null, 2));
+
+
+}
+
+
+
 
